7e08e407565b17328d9c79c79ec1bf94e7a3276c
jason <jasondeng107@gmail.com>
2023-12-12 15:15:35 -0500

    new website

---
+++ index.js
@@ -0,0 +1,35 @@
+// smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+// get time/date
+
+function updateClock() {
+
+    const now = new Date(); //current date
+
+    let hour = now.getHours() % 12; // get current hour
+
+    let minute = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+
+    let seconds = now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+    let pmoram = now.getHours() > 12 ? "PM" : "AM";
+
+
+
+    document.getElementById("time").innerHTML = "Time is currently: " + hour + ":" + minute + ":" + seconds +  " " + pmoram ;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
