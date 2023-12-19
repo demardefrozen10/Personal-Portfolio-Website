006f495ce6d5284a854d5875a9f5e23718636373
jason <jasondeng107@gmail.com>
2023-12-19 00:32:40 -0500

    add interactive navbar for pc and mobile

--- index.js
+++ index.js
@@ -36,12 +36,15 @@ setInterval(updateClock, 1000);
 
 
 
-function navBar() {
-    var x = document.getElementById("ok");
-    if (x.style.display === "block") {
-        x.style.display = none;
-    }
-    else {
-        x.style.display = "block";
-    }
-}
+const navEL = document.querySelector('.nav');
+const hamburgerEL = document.querySelector('.hamburger');
+
+hamburgerEL.addEventListener('click', () => {
+    navEL.classList.toggle("nav--open");
+    hamburgerEL.classList.toggle("hamburger--open");
+});
+
+navEL.addEventListener("click", () => {
+    navEL.classList.remove("nav--open");
+    hamburgerEL.classList.remove("hamburger--open");
+})
