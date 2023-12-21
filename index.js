b9e0651614fc2f2a411bccd9e32050585503352c
jason <jasondeng107@gmail.com>
2023-12-21 15:18:56 -0500

    fix date

--- index.js
+++ index.js
@@ -32,7 +32,18 @@ function updateClock() {
         "July", "August", "September", "October", "November", "December"
     ];
     document.getElementById("time").innerHTML = "Time is currently: " + hour + ":" + minute + ":" + seconds +  " " + pmoram ;
-    document.getElementById("today").innerHTML = days[now.getDay()] + ", " +monthNames[now.getMonth()] + " " + now.getDay();
+    function getOrdinal(number) {
+        var suffix = "th";
+        if (number % 10 === 1 && number % 100 !== 11) {
+            suffix = "st";
+        } else if (number % 10 === 2 && number % 100 !== 12) {
+            suffix = "nd";
+        } else if (number % 10 === 3 && number % 100 !== 13) {
+            suffix = "rd";
+        }
+        return number + suffix;
+    }
+    document.getElementById("today").innerHTML = days[now.getDay()] + ", " +monthNames[now.getMonth()] + " " + getOrdinal(now.getDate());
 }
 
 updateClock();
